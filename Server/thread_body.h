4fdcbd6a6577b6763c44b0f93ebb5a9da4a35eef
Paolo <s265525@studenti.polito.it>
2020-05-06 16:50:00 +0200

    Moved from run to execute job

--- Server/thread_body.h
+++ Server/thread_body.h
@@ -4,6 +4,17 @@
 #include <QObject>
 #include <QTcpSocket>
 #include "database/collegamentoDB.h"
+#include <QMutex>
+
+extern QMutex* mutex_users;
+extern QMutex* mutex_docs;
+extern QMutex* mutex_workingUsers;
+extern QMutex* mutex_db;
+
+extern QMap<QString, int> users;
+extern QMap<QString, int> documents;
+extern QMap<int, QVector<int>> workingUsers;
+extern QString path;
 
 class Thread_body : public QObject
 {
@@ -15,7 +26,22 @@ public:
 private:
     CollegamentoDB* database;
     QDataStream* in;
-
+    void create(QString username, QString password, QString nickname, QString icon);
+    void login(QString username, QString password);
+    void update(int userId, QString password, QString nickname, QString icon);
+    void getUsername(int userId);
+    void getNickname(int userId);
+    void getIcon(int userId);
+    void getDocs(int userId);
+    void newDoc(QString docName, int userId);
+    void getDocumentDatoUri(QString uri);
+    void getUri(int docId);
+    void getDocName(int docId);
+    void deleteDoc(int userId, int docId);
+    void openDoc(int docId, int userId);
+    void getWorkingUsersGivenDoc(int docId);
+    int addToWorkingUsers(int docId, int userId, int open_new);
+    int removeFromWorkingUsers(int docId, int userId);      // da implementare e da usare
 signals:
     void error(QTcpSocket::SocketError socketError);
 
