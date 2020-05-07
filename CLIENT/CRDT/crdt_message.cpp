936021e17ed7f25d9aaa9e30e4d18918393d429b
Ilaria <s257312@studenti.polito.it>
2020-05-07 10:41:11 +0200

    Aggiunti overload operatori << e >> nella classe CRDT_Message
    Sia in CLIENT sia in Server

--- CLIENT/CRDT/crdt_message.cpp
+++ CLIENT/CRDT/crdt_message.cpp
@@ -14,3 +14,39 @@ CRDT_Symbol CRDT_Message::getSimbolo() const{
 int CRDT_Message::getCreatore() const{
     return this->creatore;
 }
+
+void CRDT_Message::setAzione(std::string azione){
+    this->azione = azione;
+}
+
+void CRDT_Message::setSimbolo(CRDT_Symbol s){
+    this->simbolo = s;
+}
+
+void CRDT_Message::setCreatore(int c){
+    this->creatore = c;
+}
+
+QDataStream& operator<<(QDataStream& s, const CRDT_Message& m){
+    QString str = QString::fromStdString(m.getAzione());
+
+    // Invio sul QDataStream gli oggetti nell'ordine:     QString --> CRDT_Symbol --> int
+    s << str.toUtf8() << m.getSimbolo() << m.getCreatore();
+
+    return s;
+}
+
+QDataStream& operator>>(QDataStream& s, CRDT_Message& m){
+    QByteArray azione;
+    CRDT_Symbol simbolo;
+    int creatore;
+
+    // Ricevo sul QDataStream gli oggetti nell'ordine:     QString --> CRDT_Symbol --> int
+    s >> azione >> simbolo >> creatore;
+
+    m.setAzione(azione.toStdString());
+    m.setSimbolo(simbolo);
+    m.setCreatore(creatore);
+
+    return s;
+}
--- Server/crdt/crdt_message.h
+++ Server/crdt/crdt_message.h
@@ -9,6 +9,9 @@ class CRDT_Symbol;
 
 class CRDT_Message
 {
+    friend QDataStream & operator<<(QDataStream &, const CRDT_Message &);
+    friend QDataStream & operator>>(QDataStream &, CRDT_Message &);
+
 private:
     std::string azione;
     CRDT_Symbol simbolo;
@@ -20,6 +23,9 @@ public:
     std::string getAzione() const;
     CRDT_Symbol getSimbolo() const;
     int getCreatore() const;
+    void setAzione(std::string azione);
+    void setSimbolo(CRDT_Symbol s);
+    void setCreatore(int c);
 };
 
 Q_DECLARE_METATYPE(CRDT_Message)
