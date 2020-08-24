1219f375f059aaff59f381bdf26f453ef52cba5c
Mirko <mirkosatragno@gmail.com>
2020-08-24 23:21:30 +0200

    Even more GUI code clean up
    Mi sono accorto che alla fine certe funzioni non venivano usate, per motivi vari

--- CLIENT/GUI/editorWindow/gui_colorsmanager.h
+++ CLIENT/GUI/editorWindow/gui_colorsmanager.h
@@ -5,23 +5,26 @@
 #include <QColor>
 #include <QStack>
 
+/*L'idea era di avere un allocatore di colori, ma poi gli utenti sono diventati contributors a vita*/
+
 class GUI_ColorsManager : public QObject
 {
     Q_OBJECT
 public:
     explicit GUI_ColorsManager();
-    QColor *takeColor();
-    void returnColor(QColor *color);
+    //QColor *takeColor();
+    //void returnColor(QColor *color);
+
+    //genera nuovi colori quando lo stack è vuoto
+    QColor *newColor();
 
 private:
-    QStack<QColor*> availableColors;
+    //QStack<QColor*> availableColors;
     //sfrutto il modello HSV
     int h;
     int sFlag;
     int vFlag;
 
-    //genera nuovi colori quando lo stack è vuoto
-    QColor *newColor();
 };
 
 #endif // GUI_COLORSMANAGER_H
