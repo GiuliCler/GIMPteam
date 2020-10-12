3ef6c3e296d378573c5bc4c8387d52d4f552451a
Mirko <mirkosatragno@gmail.com>
2020-10-12 20:07:58 +0200

    Local insert speed-up

--- CLIENT/CRDT/crdt_controller.cpp
+++ CLIENT/CRDT/crdt_controller.cpp
@@ -493,13 +493,15 @@ void CRDT_controller::contentChanged(int pos, int del, int add){
         firstPosition.push_back(crdt.getSiteId());
         firstPosition.push_back(crdt.getSiteId());
 
+        QString document = textEdit.toPlainText();
+
         // Insert in crdt, making sure that the font size is > 0 (or use the default one)
         for(int i = pos+1; i < pos + add; ++i){
             tmp.movePosition(QTextCursor::NextCharacter);
             QTextCharFormat fmt{tmp.charFormat()};
 
             firstPosition[firstPosition.size()-2]++;
-            crdt.localInsert(i, textEdit.toPlainText().at(i), fmt, tmp.blockFormat().alignment(), firstPosition);
+            crdt.localInsert(i, document.at(i), fmt, tmp.blockFormat().alignment(), firstPosition);
         }
     }
 
